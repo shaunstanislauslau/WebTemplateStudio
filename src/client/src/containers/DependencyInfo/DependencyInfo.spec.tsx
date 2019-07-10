b01ff53ca537e74e7b3f3feb11b4951d18ced912
Jason Doo <t-jedoo@microsoft.com>
2019-07-10 11:26:03 -0700

    div tests

--- src/client/src/containers/DependencyInfo/DependencyInfo.spec.tsx
+++ src/client/src/containers/DependencyInfo/DependencyInfo.spec.tsx
@@ -2,13 +2,33 @@ import * as React from "react";
 import { shallow } from "enzyme";
 import { WIZARD_CONTENT_INTERNAL_NAMES } from "../../utils/constants";
 import DependencyInfo, { dependencies } from "./index";
+import { ReactComponent as Warning } from "../../assets/warning.svg";
+import { ReactComponent as Checkmark } from "../../assets/checkgreen.svg";
 
 describe("DependencyInfo", () => {
   let props: any;
   let wrapper: any;
+  let mockDependenciesStore = {
+    dependencies: {
+      node: {
+        installed: true
+      },
+      python: {
+        installed: false
+      }
+    }
+  };
+
+  // const dependencyMessageInstalled: string = intl.formatMessage(
+  //   messages.installed,
+  //   {
+  //     dependencyName: WIZARD_CONTENT_INTERNAL_NAMES.FLASK
+  //   }
+  // );
 
   beforeEach(() => {
     props = {
+      dependenciesStore: mockDependenciesStore,
       frameworkName: WIZARD_CONTENT_INTERNAL_NAMES.FLASK,
       intl: global.intl
     };
@@ -17,5 +37,14 @@ describe("DependencyInfo", () => {
 
   it("renders without crashing", () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it("is a button div", () => {
+    expect(wrapper.at(0).is("button"));
+  });
+
+  it("has an image div", () => {
+    const imgDiv = wrapper.at(0).at(0);
+    expect(imgDiv.is("img"));
   });
 });
