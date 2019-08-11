f71966a22f253c8be63248c50c47f8ece398f792
Jasmin Goh <t-ligoh@microsoft.com>
2019-08-11 00:28:49 -0700

    Hacky command to open readme

--- src/extension/src/extension.ts
+++ src/extension/src/extension.ts
@@ -1,12 +1,30 @@
-import * as vscode from "vscode";
+import { commands, ExtensionContext, Uri, workspace, window } from "vscode";
+import * as path from "path";
 import { Controller } from "./controller";
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   context.subscriptions.push(
-    vscode.commands.registerCommand(
+    commands.registerCommand(
       "webTemplateStudioExtension.wizardLaunch",
       async () => {
         Controller.getInstance(context, Date.now());
+      }
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand(
+      "webTemplateStudioExtension.openReadme",
+      async () => {
+        const workspaceInfo = workspace.workspaceFolders;
+        if (workspaceInfo) {
+          const settingsPath = path.join(
+            workspaceInfo[0].uri.fsPath,
+            "README.md"
+          );
+          const doc = await workspace.openTextDocument(Uri.file(settingsPath));
+          await window.showTextDocument(doc);
+          commands.executeCommand("markdown.showPreview");
+        }
       }
     )
   );
